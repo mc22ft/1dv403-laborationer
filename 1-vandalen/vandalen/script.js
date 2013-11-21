1af4be7a45d3a835a968d0776d52b0ec191aabf7
mc22ft <mc22ft@student.lnu.se>
2013-11-21 17:03:58 +0100

    försöker....

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,76 +2,73 @@
 
 var persArr = [{name: "Mathias Claesson", age: 35}, {name: "Neo Oskooei Claesson", age: 5}, {name: "Tolou Oskooei", age: 32}];
 
-var makePerson = function(persArr) {
-   
-    console.log("Testar om det kommer ut");
+var makePerson = function (persArr) {   
 
     //Resultat objekt
-    var result = {          
-        minAge : getMinAge(),
-        maxAge : getMaxAge(),
-    avarageAge : getAvarageAge(),
-         names : getNames()          
+    var result = {
+        minAge: getMinAge(),
+        maxAge: getMaxAge(),
+        avarageAge: getAvarageAge(),
+        names: getNames()
     };
 
-//Får ut minsta åldern
-function getMinAge(){            
-      persArr.sort(function(a, b){
-      return a.age-b.age
-      })            
-      return persArr[0].age;    
-}
-//Får ut högsta åldern
-function getMaxAge(){            
-    var tempMaxArr;
-    tempMaxArr = persArr.length;
-    return persArr[tempMaxArr];  
-}
-//Får ut medelåldern
-function getAvarageAge(){
-    var sum;
-    var i;
-    for (i = 0; i < persArr.length; i++) {
-        sum += persArr[i].age;     
+
+    console.log(result.minAge); //TEST
+
+    //Får ut minsta åldern
+    function getMinAge() {
+        persArr.sort(function (a, b) {
+            return a.age - b.age;
+        });
+        return persArr[0].age;
     }
-    return sum;                 
-}
-//Får ut namnen med ", "mellan och i bokstavsordning
-function getNames(){
-    
-}
 
+    console.log(result.maxAge); //TEST
 
-        //Får ut minsta åldern...
-        console.log(persArr[0].age); //TEST
-        
-        minAge = function outMinAge(){            
-            persArr.sort(function(a, b){
-            return a.age-b.age
-            })            
-            return persArr[0].age;    
-        }    
-        console.log(minAge); //TEST        
+    //Får ut högsta åldern
+    function getMaxAge() {
+        var tempMaxArr;
+        tempMaxArr = persArr.length;
+        return persArr[tempMaxArr];
+    }
 
-        
-        maxAge = function outMaxAge(){            
-            var tempMaxArr;
-            tempMaxArr = persArr.length;
-            return persArr[tempMaxArr];  
-        }
-
-
-        var i;    
+    //Får ut medelåldern
+    function getAvarageAge() {
+        var sum;
+        var i;
         for (i = 0; i < persArr.length; i++) {
-            console.log(persArr[i].name);
+            sum += persArr[i].age;
         }
-    
-        
+        return sum;
+    }
 
-  
+    //Får ut namnen med ", "mellan och i bokstavsordning
+    function getNames() {
+            persArr.sort("name");
+            return persArr.toString("name");
+    }
 
-//var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+    console.log(result);
+    return result;
+};
 
-//var result = makePerson(data);
 
-//console.log(result);
+
+/*
+
+var makePerson = function (persArr) {
+    var result = {};
+
+    // Lös uppgiften
+
+    return result;
+}
+
+
+var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46 }];
+
+var result = makePerson(data);
+
+console.log(result);
+
+*/
