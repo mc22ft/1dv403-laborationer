8df3bcc9302bb8bc02e494f1c84415c49a2bb910
mc22ft <mc22ft@student.lnu.se>
2013-11-25 10:32:15 +0100

    klar med steg 1

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -10,95 +10,31 @@ var makePerson = function (persArr) {
 
     //Resultat objekt
     var result = {};
-
-    /* result.minAge = minAge;
-     result.maxAge = maxAge;
-     result.avarageAge = avarageAge;
-     result.names = names;
-     */
     
-    //Får ut minsta åldern
+        //Får ut minsta åldern
          persArr.sort(function (a, b) {
              return a.age - b.age;
          });
          result.minAge = persArr[0].age;
      
-    //Får ut högsta åldern
-         
-             var tempMaxArr;
-             tempMaxArr = (persArr.length -1);
-             result.maxAge = persArr[tempMaxArr].age;
-
-    //Får ut medelåldern
-            
-                 var sum = 0;
-                 var i;
-                 for (i = 0; i < persArr.length; i++) {
-                     sum += persArr[i].age;
-                     }
-                 result.avarageAge = Math.round(sum / persArr.length);
-
-    //Får ut namnen med ", "mellan och i bokstavsordning
-                
-                   result.names = persArr.sort("name").join(", ");
-                  /*   console.log(persArr["name"]);
-                     result.names = persArr("name"); 
-                     */
-                 
-             
-
-  /*
-     minAge = getMinAge(persArr);
-     console.log(minAge);
-
-     maxAge = getMaxAge(persArr);
-     console.log(maxAge);
-
-     avarageAge = getAvarageAge(persArr);
-     console.log(avarageAge);
-
-     names = getNames(persArr);
-     console.log(names);
-
-    //Får ut minsta åldern
-    function getMinAge(pArr) {
-        persArr.sort(function (a, b) {
-            return a.age - b.age;
-        });
-        return persArr[0].age;
-    }
-
-
-    //Får ut högsta åldern
-    function getMaxAge(pArr) {
+        //Får ut högsta åldern         
         var tempMaxArr;
         tempMaxArr = (persArr.length -1);
-        return persArr[tempMaxArr].age;
-    }
+        result.maxAge = persArr[tempMaxArr].age;
 
-
-    //Får ut medelåldern
-    function getAvarageAge(pArr) {
+        //Får ut medelåldern  och får namnen till arr          
         var sum = 0;
         var i;
+        var nameArr = [];
         for (i = 0; i < persArr.length; i++) {
             sum += persArr[i].age;
-            
-        }
-        return Math.round(sum / persArr.length);
-    }
+            nameArr[i] = persArr[i].name;
+            }
+        result.avarageAge = Math.round(sum / persArr.length);
 
-
-
-
-    //Får ut namnen med ", "mellan och i bokstavsordning
-    function getNames(pArr) {
-            persArr.sort("name").join(", ");
-            console.log(persArr["name"]);
-            return persArr("name");
-    }
-    
-    */
+        //Får ut namnen med ", "mellan                 
+        
+        result.names = nameArr.join(", ");
 
     //avslutningen
 
