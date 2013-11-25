55863b3f36b0759295ed882f1c9438ace3d2a0df
mc22ft <mc22ft@student.lnu.se>
2013-11-25 15:26:26 +0100

    exta uppgift steg 1

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,67 @@
 "use strict";
 
 var makePerson = function (persArr) {
-
+    var result = {};
     // Lös uppgiften    
 
+    //Får ut minsta åldern
+
+    //ålder till array
+       
+    var myYearArr = [];
+    myYearArr = persArr.map(function (item, i) {
+        return item.age;
+    });
+
+    result.averageAge = persArr.map(function (value) {
+        return Math.round(item / persArr.length);
+    });
+    
+   /* persArr.sort(function (a, b) {
+        return a.age - b.age;
+    });
+    result.minAge = persArr[0].age;*/
+
+    //Får ut högsta åldern         
+    var tempMaxArr;
+    tempMaxArr = (persArr.length - 1);
+    result.maxAge = persArr[tempMaxArr].age;
+
+    //Får ut medelåldern  och får namnen till arr          
+    var sum = 0;
+    var i;
+    var nameArr = [];
+    for (i = 0; i < persArr.length; i++) {
+        sum += persArr[i].age;
+        nameArr[i] = persArr[i].name;
+    }
+    result.averageAge = Math.round(sum / persArr.length);
+
+    //Får ut namnen med ", "mellan                 
+
+    result.names = nameArr.join(", ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //avslutningen
+
+    // console.log(result);
+    return result;
+};
+
+    /*
     //Resultat objekt
     var result = {};
     
@@ -31,13 +89,17 @@ var makePerson = function (persArr) {
         //Får ut namnen med ", "mellan                 
         
         result.names = nameArr.join(", ");
+       
+
 
     //avslutningen
 
    // console.log(result);
     return result;
-};
 
+     
+};
+*/
 
 var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46 }];
 
