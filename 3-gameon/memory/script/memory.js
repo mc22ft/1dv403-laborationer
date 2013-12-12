14688378990861251d0545d6ddd0eb15edbb7fde
mc22ft <mc22ft@student.lnu.se>
2013-12-12 13:21:25 +0100

    Strukturen klar.

---
+++ 3-gameon/memory/script/memory.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var Memory = {
+
+	init: function () {
+
+		var myArray = [3,4,4,1,2,1,2,3];
+
+		console.log(myArray);
+
+
+
+	},
+//---Functioner ---
+
+
+
+
+
+
+
+
+
+
+
+};
+window.onload = Memory.init;
