6e20b8a77e6aa73d16378a80549ff8a7ff03cdc9
mc22ft <mc22ft@student.lnu.se>
2013-12-13 13:09:26 +0100

    Moment 8 klart!

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -18,8 +18,23 @@ var MemoryBoard = {
 		var exmpel = new Memory(myRandomArr);
 		console.log(myRandomArr);
 
+		//------ Ovanför detta är bara test!!!!!!!'
+
+
 		MemoryBoard.makeElement(myRandomArr);
 
+
+		var aLinks = document.getElementsByTagName("a");
+		console.log(aLinks);
+		
+		for(var i=0;i<aLinks.length;i++){
+      		aLinks[i].onclick= function (e){
+      			MemoryBoard.clickFunction(e);
+      			return false;
+      		};
+    	}
+
+		
 	},
 						//---Functioner ---
 
@@ -35,7 +50,9 @@ var MemoryBoard = {
 		var a = document.createElement("a");
 		a.href="#";
 		var img = document.createElement("img");
-		img.src="pics/"+nrPic+".png", img.alt="?";
+		img.setAttribute("src", "pics/0.png");
+		img.setAttribute("alt", "?");
+		//img.src="pics/"+nrPic+".png", img.alt="?";
 
 		td.appendChild(a);
 		a.appendChild(img);
@@ -46,6 +63,11 @@ var MemoryBoard = {
 		console.log(divTable);
 	},
 
+	clickFunction: function(e){
+		var e = e;
+		e.preventDefault();
+		console.log(e.target)
+	},
 
 
 
