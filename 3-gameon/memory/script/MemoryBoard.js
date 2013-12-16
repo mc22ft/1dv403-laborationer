9c13350cafdb3be2cf9ccff9bbde6e7eb1a73c56
mc22ft <mc22ft@student.lnu.se>
2013-12-16 12:03:48 +0100

    Redigerat css klar att göra

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -11,63 +11,42 @@ var mainClickCounter = 0;
 
 var MemoryBoard = {
 
-		randomArr: [],
-		
+		randomArr: [],		
 
 	init: function () {
 
 		MemoryBoard.startGame();
 
-		console.log(winCount);
-
-		
-
-		MemoryBoard.makeElement();
-
-		
+		MemoryBoard.makeElement();		
 
 		var aLinks = document.getElementsByTagName("a");
-		console.log(aLinks);
 		
 		for(var i=0;i<aLinks.length;i++){
-      		aLinks[i].onclick= function (e){
-      			
+      		aLinks[i].onclick= function (e){      			
       			if (clickCount < 3){
       				++clickCount;
       				MemoryBoard.clickFunction(e);
       			};
       			return false;
       		};
-    	}
-
-		
+    	}		
 	},
 						//---Functioner ---
 
 	clickFunction: function(e){		
-		
 		var e = e;
 		e.preventDefault();
-		//console.log("Class Namn:");
-		//console.log(e.target.parentNode.className);
-		
+	
 		var aTagNumber = parseInt(e.target.parentNode.className);
-		var oldNodeTarget = e.target;			
-		
+		var oldNodeTarget = e.target;		
 		var ArrPlace = MemoryBoard.randomArr[aTagNumber];
-
 		e.target.setAttribute("src", "pics/"+ArrPlace+".png");
 		console.log("På plars nummer " +aTagNumber+ " i Arrayen finns detta nummer: "+ArrPlace);
-		//console.log();
-
-
+	
 		MemoryBoard.compare(ArrPlace, aTagNumber, e, oldNodeTarget);
-		
-
 	},
 
-	compare: function(ArrPlace, aTagNumber, e, oldNodeTarget){
-		
+	compare: function(ArrPlace, aTagNumber, e, oldNodeTarget){	
 		
 		if (clickCount === 1){
 			clickOne = ArrPlace;
@@ -87,12 +66,9 @@ var MemoryBoard = {
 			//if klick så det ska funka - click1 = click2 
 			if(clickOne === clickTwo){
 				++innerWinCount;
-				++mainClickCounter;
-				console.log("Grattis! Ett par vänt.")
+				++mainClickCounter;			
 
 				if (winCount === innerWinCount){
-					console.log("Grattis du har vunnit spelet!!!");
-
 					var div = document.getElementById("divTable");
 					var p = document.createElement("p");
 					div.appendChild(p);
@@ -100,70 +76,45 @@ var MemoryBoard = {
 				
 				};
 				//Nollställer räknare 
-				clickCount = 0;	
-
+				clickCount = 0;
 			}
 			else
-				{
-					//Timer
+				{	//Timer
 					clickCount = 3;
 					++mainClickCounter;
+
 					setTimeout(function(){						
 						oldTarget.setAttribute("src", "pics/0.png");
 						e.target.setAttribute("src", "pics/0.png");
 						clickCount = 0;
-					}, 1000);
-
-					//console.log(clickOne);
-					//vända tillbaka om det inte är samma		
+					}, 1000);		
 				};			
-		};
-
-		console.log(clickCount);
-			
+		};			
 	},
-
-
 		//Gör elementen med bilderna i
 	makeElement: function(){
 		var div = document.getElementById("divTable");
 		var td = document.createElement("td");
 		var i;
 		var textNode;
+		for (i = MemoryBoard.randomArr.length - 1; i >= 0; i--) {		
+			var a = document.createElement("a");
+			a.className =i;
+			a.href="#";		
 
-		for (i = MemoryBoard.randomArr.length - 1; i >= 0; i--) {			
-		//var nrPic = myRandomArr[i];
-		var a = document.createElement("a");
-		a.className =i;
-		a.href="#";
-		//textNode = document.createTextNode("X");
-		//a.appendChild(textNode);
-
-		var img = document.createElement("img");
-		img.setAttribute("src", "pics/0.png");
-		img.setAttribute("alt", "?");
-		//img.src="pics/"+nrPic+".png", img.alt="?";
-
-		td.appendChild(a);
-		a.appendChild(img);
-
+			var img = document.createElement("img");
+			img.setAttribute("src", "pics/0.png");
+			img.setAttribute("alt", "?");
+			td.appendChild(a);
+			a.appendChild(img);
 		};
 
-
-		div.appendChild(td);
-		
-		console.log(divTable);
-
+		div.appendChild(td);		
 	},
 
 	startGame: function(){
 
-		//var myArray = [3,4,4,1,2,1,2,3]; 
-
-		//console.log(myArray);
-
 		MemoryBoard.randomArr = RandomGenerator.getPictureArray(4, 4);
-
 		console.log(MemoryBoard.randomArr);
 
 		//Skapa ojekt
@@ -172,15 +123,6 @@ var MemoryBoard = {
 
 		//Ställer in räknare så att spelet vet när det är klart.
 		winCount = MemoryBoard.randomArr.length / 2;
-
 	},
-
-
-
-
-
-
-
-
 };
 window.onload = MemoryBoard.init;
